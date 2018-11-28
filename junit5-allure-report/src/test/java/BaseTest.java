f4afa8103c1eee0709afaee1d6f0c742d89aa0c7
nietoalba <nietoalba@gmail.com>
2018-11-28 12:19:31 +0100

    allureExample

---
+++ junit5-allure-report/src/test/java/BaseTest.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.PageGenerator;
+
+public class BaseTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public PageGenerator page;
+
+    @BeforeEach
+    public void classLevelSetup() {
+		 //System.setProperty("webdriver.chrome.driver","");
+			System.setProperty("webdriver.chrome.driver", "C:\\_ej_ico_0\\Selenium 22_10_2018\\chromedriver.exe");
+
+		 
+		
+		 //headless chrome
+		ChromeOptions options = new ChromeOptions();
+	        options.addArguments("headless");
+	      //  options.addArguments("window-size=1200x600");
+	        options.addArguments("--no-sandbox");
+	        options.addArguments("--disable-dev-shm-usage");
+	        
+		    driver = new ChromeDriver(options);
+		  //  baseUrl = "https://www.ico.es/";
+		  //  driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		    
+    	
+        //driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,15);
+        page = new PageGenerator(driver);
+    }
+
+    @AfterEach
+    public void teardown () {
+        driver.quit();
+    }
+}
