299d58fe81dd2429ff66167bf95c5b5a5aff290a
nietoalba <nietoalba@gmail.com>
2018-12-03 13:26:33 +0100

    unit

---
+++ junit5-allure-report/src/test/java/UnitTest.java
@@ -0,0 +1,29 @@
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Epic("Unit Testing")
+@Feature("Unit testing Fase 1")
+
+public class UnitTest {
+
+	@Test
+    @Story("test unitario 1")
+    @Description("test unitario 1. descripcion")
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+    	//UnitTest tester = new UnitTest(); // MyClass is tested
+
+        // assert statements
+        assertEquals(0, 0, "10 x 0 must be 0");
+       
+    }
+}
+
